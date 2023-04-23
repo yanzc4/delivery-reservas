04df1267fc01cbc43fac55cbd9dded9b3a709588
Saul V3 <felix21soc@gmail.com>
2023-04-23 17:08:33 -0500

    Primer Panel compeltado
    Listo

---
+++ Html/index.js
@@ -0,0 +1,42 @@
+const sideMenu =document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
+
+
+//mostrar cabecera
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+//cerrar cabecera
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+//cambiar el tema
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+});
+
+//lennar con datos la tabla
+Orders.forEach((order) => {
+    const tr = document.createElement('tr');
+    const trContent = `	
+    <td>${order.producName}</td>
+    <td>${order.producNumber}</td>
+    <td>${order.paymenStatus}</td>
+    <td>"${order.shipping === 'Declined' ? 'danger' :
+    order.shipping=== 'pending' ? 'warning': 'primary'}">
+    ${order.shipping}</td>
+    <td class="primary">Detalles</td>
+    `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+}); 
+
---
+++ Html/orders.js
@@ -0,0 +1,32 @@
+const Order =[
+    {
+        productName: 'Coca Cola',
+        productNumber:'8153',
+        paymenStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName: 'Pepsi',
+        productNumber:'8154',
+        paymenStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName: 'Fanta',
+        productNumber:'8155',
+        paymenStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName: 'Sprite',
+        productNumber:'8156',
+        paymenStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName: '7up',
+        productNumber:'8157',
+        paymenStatus:'Due',
+        shipping:'Pending',
+    },
+]
