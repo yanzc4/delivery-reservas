b84b66ed276d9513e602bdf3d61f72aa7b8c7a6a
Yan Zapata Cardoza <yanzapatac@gmail.com>
2023-06-11 17:52:21 -0500

    compleatdo al 99% con Chatbot

--- cliente/index.php
+++ cliente/index.php
@@ -1,5 +1,5 @@
 <?php
-$cabecera="Delivery";
+$cabecera = "Delivery";
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -15,34 +15,41 @@ $cabecera="Delivery";
 </head>
 
 <body>
-<?php require_once('../frontend/menucliente.php') ?>
+    <?php require_once('../frontend/menucliente.php') ?>
 
-<section class="home">
-    <iframe src="platos.php" name='myFrame' style="height: 100%; width: 100%; border: none;"></iframe>
-    
+    <section class="home">
+        <iframe src="platos.php" name='myFrame' style="height: 100%; width: 100%; border: none;"></iframe>
+
         <!--<div class="text">Dashboard Sidebar</div>-->
     </section>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="../assets/js/menu/funcionmenu.js"></script>
     <script>
-window.addEventListener('load', initLandbot, { once: true });
-window.addEventListener('touchstart', initLandbot, { once: true });
-var myLandbot;
-function initLandbot() {
-  if (!myLandbot) {
-    var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
-    s.addEventListener('load', function() {
-      var myLandbot = new Landbot.Livechat({
-        configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-1578347-AISGTW9VLKEFK7Z1/index.json',
-      });
-    });
-    s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
-    var x = document.getElementsByTagName('script')[0];
-    x.parentNode.insertBefore(s, x);
-  }
-}
-</script>
+        window.addEventListener('load', initLandbot, {
+            once: true
+        });
+        window.addEventListener('touchstart', initLandbot, {
+            once: true
+        });
+        var myLandbot;
+
+        function initLandbot() {
+            if (!myLandbot) {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.addEventListener('load', function() {
+                    var myLandbot = new Landbot.Livechat({
+                        configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-1578347-AISGTW9VLKEFK7Z1/index.json',
+                    });
+                });
+                s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+            }
+        }
+    </script>
 </body>
 
 </html>
